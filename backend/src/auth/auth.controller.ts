dd01291af9afe8baff5815890eecdf474bd2858c
artmigalev <art_migalev@yahoo.com>
2026-05-23 12:47:29 +0300

    --amend

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,33 @@
+import { AuthService } from '@auth/auth.service';
+import { LoginDto } from '@/src/auth/dto/login.dto';
+import { RegisterDto } from '@dto/register.dto';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Inject,
+  Post,
+  Res,
+} from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Get('/me')
+  getCredentials() {
+    return this.authService.getUser();
+  }
+  // /auth/register
+  @Post('/register')
+  createUser(@Body() userCredentials: RegisterDto) {
+    return this.authService.userRegister(userCredentials);
+  }
+
+  // /auth/login
+  @Post('/login')
+  authenticateUser(@Body() userCredentials: LoginDto) {
+    return this.authService.userLogin(userCredentials);
+  }
+}
---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { LoginDto } from '@/src/auth/dto/login.dto';
+import { RegisterDto } from '@/src/auth/dto/register.dto';
+import { Body, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  getUser() {
+    return 'Current user data';
+  }
+
+  userRegister(userCredentials: RegisterDto): string {
+    return userCredentials.email;
+  }
+
+  userLogin(userCredentials: LoginDto): string {
+    return userCredentials.email;
+  }
+}
