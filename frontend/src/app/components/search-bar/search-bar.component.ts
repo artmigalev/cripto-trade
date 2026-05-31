53c1a9e3a1b757c3b603835f709b24d9db9758cb
artmigalev <art_migalev@yahoo.com>
2026-05-31 12:12:14 +0300

    feat: search debounce

--- frontend/src/app/components/search-bar/search-bar.component.ts
+++ frontend/src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MarketService } from '@services/market.service';
+import { debounceTime, distinctUntilChanged, map, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,4 +9,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './search-bar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SearchBarComponent {}
+export class SearchBarComponent {
+  private readonly marketService = inject(MarketService);
+
+  private readonly subjectSearch = new Subject<Event>();
+
+  private search$ = this.subjectSearch.pipe(
+    map(event => (event.target as HTMLInputElement).value),
+    debounceTime(300),
+    distinctUntilChanged()
+  );
+
+  onSearch(event: Event) {
+    this.subjectSearch.next(event);
+
+    this.search$.subscribe(value => this.marketService.setSearch(value));
+  }
+}
