8d7569f3ed71ef9cf3522c307b26bc2c73829835
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2026-06-08 18:20:38 +0300

    test:app- should create the

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,17 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideRouter } from '@angular/router';
+import { FooterComponent } from '@components/footer/footer.component';
+import { HeaderComponent } from '@components/header/header.component';
+import { AuthService } from '@services/auth.service';
+import { API_CONFIG } from '@services/tokens/api-config.tokens';
 
 describe('App', () => {
-  // beforeEach(async () => {
-  //   await TestBed.configureTestingModule({
-  //     imports: [App],
-  //   }).compileComponents();
-  // });
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(App);
-  //   const app = fixture.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
+  let title: () => string;
+  beforeEach(async () => {
+    const mockAuthService = {
+      isAuthenticated: () => false,
+      isApiConfigured: () => false,
+    };
+
+    await TestBed.configureTestingModule({
+      imports: [App],
+      providers: [
+        provideRouter([]),
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+        {
+          provide: API_CONFIG,
+          useValue: {
+            baseUrl: '/api',
+            wsUrl: 'wss://ws-api.testnet.binance.vision/ws-api/v3',
+            backendUrl: 'http://localhost:3000',
+          },
+        },
+      ],
+    }).compileComponents();
+  });
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(app).toBeTruthy();
+  });
   // it('should render title', async () => {
   //   const fixture = TestBed.createComponent(App);
   //   await fixture.whenStable();
