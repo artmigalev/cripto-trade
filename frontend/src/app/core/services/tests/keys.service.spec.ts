69c358cfaf08e7765e1f0a6c6e6ea1951f2d40f9
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2026-06-09 22:32:04 +0300

    test: keys service saved keys && returned   typeof ResponseKey

--- frontend/src/app/core/services/tests/keys.service.spec.ts
+++ frontend/src/app/core/services/tests/keys.service.spec.ts
@@ -4,6 +4,7 @@ import { TestBed } from '@angular/core/testing';
 import { AuthService } from '@services/auth.service';
 import { API_CONFIG } from '@services/tokens/api-config.tokens';
 import { KeysService } from '@services/keys.service';
+import { PayloadKeys } from '@interfaces/keys.interface';
 
 describe('Keys Service', () => {
   let keyService: KeysService;
@@ -27,7 +28,9 @@ describe('Keys Service', () => {
 
     httpMock = TestBed.inject(HttpTestingController);
   });
-
+  afterEach(() => {
+    httpMock.verify();
+  });
   it('should return key and status', async () => {
     const keys = { apiKey: 'apiKey', configured: true };
     const promise = keyService.getKeys();
@@ -37,5 +40,16 @@ describe('Keys Service', () => {
     request.flush(keys);
 
     expect(await promise).toEqual(keys);
+  });
+  it('should returned  key and status', async () => {
+    const result = { apiKey: 'apiKey', configured: true };
+    const payload: PayloadKeys = { apiKey: 'apiKey', secretKey: 'secretKey' };
+    const promise = keyService.saveKeys(payload);
+    const request = httpMock.expectOne(`http://localhost:3000/keys`);
+
+    expect(request.request.method).toBe('PUT');
+    request.flush(result);
+
+    expect(await promise).toEqual(result);
   });
 });
