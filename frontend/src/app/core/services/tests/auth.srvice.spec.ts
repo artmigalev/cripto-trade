8350be208be0d4e66b86ea4b651005dc76c19563
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2026-06-09 18:46:53 +0300

    test: authService should authenticate user

---
+++ frontend/src/app/core/services/tests/auth.srvice.spec.ts
@@ -0,0 +1,57 @@
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { AuthService } from '@services/auth.service';
+import { API_CONFIG } from '@services/tokens/api-config.tokens';
+
+describe('Auth Service', () => {
+  const userMock = {
+    email: 'email',
+    password: 'password',
+  };
+  const getItemSpy = vi.spyOn(Storage.prototype, 'getItem');
+  const setItemSpy = vi.spyOn(Storage.prototype, 'setItem');
+  let authService: AuthService;
+  let httpMock: HttpTestingController;
+  beforeEach(() => {
+    localStorage.clear();
+    getItemSpy.mockClear();
+    setItemSpy.mockClear();
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: API_CONFIG,
+          useValue: {
+            baseUrl: '/api',
+            wsUrl: 'wss://ws-api.testnet.binance.vision/ws-api/v3',
+            backendUrl: 'http://localhost:3000',
+          },
+        },
+      ],
+    });
+    authService = TestBed.inject(AuthService);
+
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    localStorage.clear();
+    getItemSpy.mockClear();
+    setItemSpy.mockClear();
+    httpMock.verify();
+  });
+
+  it('should authenticate user and store access token', async () => {
+    const promise = authService.login(userMock.email, userMock.password);
+
+    const request = httpMock.expectOne(`http://localhost:3000/auth/login`);
+    request.flush({ access_token: 'token' });
+    await promise;
+
+    expect(request.request.body).toEqual(userMock);
+    expect(request.request.method).toBe('POST');
+    expect(authService.isAuthenticated()).toBe(true);
+  });
+});
