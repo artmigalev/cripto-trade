c73d2d40998a3f3e2084fca2594c444b407ab211
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2026-06-10 20:04:29 +0300

    init: chart

---
+++ frontend/src/app/interfaces/chart.interface.ts
@@ -0,0 +1,38 @@
+import { CandleIntervals } from '@enums/trade.enum';
+
+export interface Chart {
+  klines: Kline[];
+  candleIntervals: CandleIntervals[];
+  activeCandleInterval: CandleIntervals;
+}
+
+export interface Kline {
+  openTime: number;
+  openPrice: string;
+  highPrice: string;
+  lowPrice: string;
+  closePrice: string;
+  volume: string;
+  closeTime: number;
+  quoteAssetVolume: string;
+  numberOfTrades: number;
+  takerBuyBaseAssetVolume: string;
+  takerBuyQuoteAssetVolume: string;
+}
+
+// [
+//   [
+//     1499040000000, // Kline open time
+//     '0.01634790', // Open price
+//     '0.80000000', // High price
+//     '0.01575800', // Low price
+//     '0.01577100', // Close price
+//     '148976.11427815', // Volume
+//     1499644799999, // Kline Close time
+//     '2434.19055334', // Quote asset volume
+//     308, // Number of trades
+//     '1756.87402397', // Taker buy base asset volume
+//     '28.46694368', // Taker buy quote asset volume
+//     '0', // Unused field, ignore.
+//   ],
+// ];
