98e0272c000923e6ead2eefc12772c5362e73d36
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2026-07-04 15:31:44 +0300

    fix: :bug: computed heade links

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -7,11 +7,16 @@ import {
 import { NavigationComponent } from '@components/navigation/navigation.component';
 import { SearchBarComponent } from '@components/search-bar/search-bar.component';
 import { StatusIconComponent } from '@components/status-icon/status-icon.component';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  NavigationStart,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
 import { AuthRotes, NavLink } from '@enums/nav-link.enum';
 import { MatListItem, MatNavList } from '@angular/material/list';
 import { AuthService } from '@services/auth.service';
-import { HeaderNavLink } from '@interfaces/header.interface';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-header',
@@ -31,18 +36,30 @@ import { HeaderNavLink } from '@interfaces/header.interface';
 })
 export class HeaderComponent {
   private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
 
   isAuthenticated = computed(() => this.authService.isAuthenticated());
   homeLink = NavLink.Dashboard.toLowerCase();
 
-  authLinks: HeaderNavLink[] = Object.entries(AuthRotes)
-    .map(([title, link]) => ({
-      title: title,
-      link: link.toLowerCase(),
-    }))
-    .filter(({ title }) =>
-      this.isAuthenticated()
-        ? title === AuthRotes.Logout
-        : title !== AuthRotes.Logout
-    );
+  authLinks = computed(() =>
+    Object.entries(AuthRotes)
+      .map(([title, link]) => ({
+        title: title,
+        link: link.toLowerCase(),
+      }))
+      .filter(({ title }) =>
+        this.isAuthenticated()
+          ? title === AuthRotes.Logout
+          : title !== AuthRotes.Logout
+      )
+  );
+  constructor() {
+    this.router.events.pipe(takeUntilDestroyed()).subscribe(event => {
+      if (event instanceof NavigationStart) {
+        if (event.url.slice(1) === AuthRotes.Logout.toLowerCase()) {
+          this.authService.logout();
+        }
+      }
+    });
+  }
 }
