762d0fc55780004b283635155a623d587a42f551
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2026-08-10 15:42:18 +0300

    feat: new order form

--- frontend/src/app/components/order-form/order-form.component.ts
+++ frontend/src/app/components/order-form/order-form.component.ts
@@ -1,10 +1,46 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+interface State {
+  side: string;
+  type: string;
+  price: string;
+  amount: string;
+}
+
+// type SchemaKeys = keyof typeof orderFormShema & string
 
 @Component({
   selector: 'app-order-form',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './order-form.component.html',
   styleUrl: './order-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OrderFormComponent {}
+export class OrderFormComponent {
+  private orderState = signal<State>({
+    side: 'Buy',
+    type: 'Market',
+    price: '',
+    amount: '',
+  });
+  ifPrice = computed(() => this.orderState().type === 'Limit');
+  protected readonly orderFormShema = {
+    side: { data: ['Buy', 'Sell'], label: 'Side' }, //'Side',
+    type: { data: ['Market', 'Limit'], label: 'Order Type' }, // 'Order Type',
+    amount: 'Amount',
+    price: 'Price',
+  } as const;
+
+  protected readonly schemaKeys: (keyof typeof this.orderFormShema)[] = [
+    'type',
+    'side',
+    'amount',
+    'price',
+  ];
+}
