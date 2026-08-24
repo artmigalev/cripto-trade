e74b72bef36c7cdaa9c573b80a0830c787a6c960
artmigalev <art_migalev@yahoo.com>
2026-08-24 21:11:46 +0300

    fix:config mode

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { AuthModule } from '@/src/auth/auth.module';
 import { KeyModule } from './key/key.module';
 import { TradeModule } from '@/src/trade/trade.module';
 import { PortfolioModule } from '@/src/portfolio/portfolio.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthModule, KeyModule, TradeModule, PortfolioModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:  join(process.cwd(), 'enviroment', `.env.${process.env.NODE_ENV || 'development'}`),
+      isGlobal: true,
+    }),
+    AuthModule,
+    KeyModule,
+    TradeModule,
+    PortfolioModule,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly configService: ConfigService) {
+    console.log(this.configService.get<string>('FRONTEND_URL'));
+    console.log('NODE_ENV:', process.env.NODE_ENV);
+  }
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,24 @@
-
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+  const frontend = config.get('FRONTEND_URL');
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true
-  }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
   app.enableCors({
-    origin: 'http://localhost:4200', // Angular dev server
+    origin: frontend, // Angular dev server
   });
 
   await app.listen(process.env.PORT ?? 3000);
